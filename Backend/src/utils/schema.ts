d3bfe10b98a6eae908b222d695e1372792c0874b
Carlos S. Ferreira <ferreira.carlos.bcs@gmail.com>
2025-03-07 02:46:03 -0300

    teste

--- Backend/src/utils/schema.ts
+++ Backend/src/utils/schema.ts
@@ -34,18 +34,10 @@ export const loginSchema = z
   })
 
 export const authCheckSchema = z.object({
-  admin: z.enum(['true', 'false'], {
-    errorMap: () => ({ message: "admin must be 'true' or 'false'" }),
-  }),
-  project: z.enum(['true', 'false'], {
-    errorMap: () => ({ message: "project must be 'true' or 'false'" }),
-  }),
-  personal: z.enum(['true', 'false'], {
-    errorMap: () => ({ message: "personal must be 'true' or 'false'" }),
-  }),
-  financial: z.enum(['true', 'false'], {
-    errorMap: () => ({ message: "financial must be 'true' or 'false'" }),
-  }),
+  admin: zodRegex('admin', /^true$|^false$/),
+  project: zodRegex('project', /^true$|^false$/),
+  personal: zodRegex('personal', /^true$|^false$/),
+  financial: zodRegex('financial', /^true$|^false$/),
 })
 
 export const authSelectSchema = z.object({
