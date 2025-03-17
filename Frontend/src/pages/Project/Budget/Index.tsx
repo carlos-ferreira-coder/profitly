9b142ee294914e6dd019538990dac3004d5a2344
Carlos S. Ferreira <ferreira.carlos.bcs@gmail.com>
2025-03-17 16:20:28 -0300

    ok

--- Frontend/src/pages/Project/Budget/Index.tsx
+++ Frontend/src/pages/Project/Budget/Index.tsx
@@ -41,7 +41,7 @@ const Budget = () => {
             {budget ? (
               <>
                 <h2 className="mb-9 text-2xl font-bold text-black dark:text-white sm:text-title-xl2">
-                  Orçamento
+                  Orçamento / {budget.project.name}
                 </h2>
 
                 <Form budget={budget} />
