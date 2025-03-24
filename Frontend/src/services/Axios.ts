552b4546f5e95ef8ffb9633b9153a62c22a10582
Carlos S. Ferreira <ferreira.carlos.bcs@gmail.com>
2025-03-24 15:31:43 -0300

    fix: .env

--- Frontend/src/services/Axios.ts
+++ Frontend/src/services/Axios.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from 'axios'
 
-export const serverURL = process.env.DOMAIN || ''
+export const serverURL = process.env.SERVER_DOMAIN || ''
 
 export const api = axios.create({
   baseURL: serverURL,
